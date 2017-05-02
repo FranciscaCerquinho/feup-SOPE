16d3be9058edbcb7c189a7c10e2a4d9fc392d31c
Beatriz de Henriques Martins <up201502858@fe.up.pt>
2017-05-02 10:51:09 +0100

    leitura argumentos

--- gerador.c
+++ gerador.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/file.h>
@@ -16,19 +18,50 @@ int readline(int fd, char *str){
 }
 
 int main(int argc, char const *argv[]) {
+
+  //tratamento dados
+  if(argc != 4){
+      printf("Usage: geardor <n. Pedidos> <max. utilizacao> <un. tempo>\n");
+      return 1;
+  }
+
+  int nPedidos=atoi(argv[1]);
+  int maxUtilizacao=atoi(argv[2]);
+  char unTempo=argv[3][0];
+
+  //
+  printf("%d %d %c\n", nPedidos,maxUtilizacao,unTempo);
+  //
+  
   //entrada
   int fdEnt;
   char str[9999];
-printf("AQUI\n");
+
+  //
+  printf("AQUI\n");
+  //
+
   fdEnt=open("/tmp/entrada",O_WRONLY);
-printf("AQUI\n");
+
+  //
+  printf("AQUI\n");
+  //
+
   //rejeitados
   int fdRej;
 
   mkfifo("/tmp/rejeitados",0660);
-printf("AQUI\n");
+
+  //
+  printf("AQUI\n");
+  //
+
   fdRej=open("/tmp/rejeitados",O_RDONLY);
-printf("AQUI\n");
+
+  //
+  printf("AQUI\n");
+  //
+
   while(readline(fdRej,str)){
     printf("%s",str);
   }
