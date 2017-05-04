4b8fc625b96c3e088fea0b6f4d0e9bef278d2a64
Francisca Cerquinho <franciscacerquinho@hotmail.com>
2017-05-04 10:44:30 +0100

    Gerador quase feito

--- gerador.c
+++ gerador.c
@@ -8,172 +8,193 @@
 #include <sys/file.h>
 #include <pthread.h>
 
+struct timespec ts;
+
 /*
  * struct request
  */
 struct request{
-  int serial_number;
-  char gender;
-  int timeReq;
-  int nRejeitados;
+	int serial_number;
+	char gender;
+	int timeReq;
+	int nRejeitados;
 };
 
 //ver passagem de parametros das threads
 
 void *thr_NewsRequest(void *arg){
-  //fifo de entrada
-  int fdEnt;
+	//input fifo
+	int fdEnt;
 
-  fdEnt = open("/tmp/entrada", O_WRONLY);
+	fdEnt = open("/tmp/entrada", O_WRONLY);
 
-  struct request geradoReq;
+	struct request geradoReq;
 
-  //auxiliar de rand
-  srand(time(NULL));
+	//assistant of rand
+	srand(time(NULL));
 
-  int cont = 1;
-  int nSerie = 1;
+	int cont = 1;
+	int nSerie = 1;
 
-  while (cont <= nPedidos) {
-    char g;
-    int t;
+	while (cont <= nPedidos) {
+		char g;
+		int t;
 
-    int auxG = rand() & 1;
+		int auxG = rand() & 1;
 
-    /**/
-    //printf("Gender= %d\n", auxG);
-    /**/
+		/**/
+		//printf("Gender= %d\n", auxG);
+		/**/
 
-    if(auxG == 0){
-      g = 'M';
-    }else{
-      g = 'F';
-    }
+		if(auxG == 0){
+			g = 'M';
+		}else{
+			g = 'F';
+		}
 
-    t = rand() % maxUtilizacao + 1;
+		t = rand() % maxUtilizacao + 1;
 
-    /**/
-    //printf("time= %d\n", t);
-    /**/
+		/**/
+		//printf("time= %d\n", t);
+		/**/
 
-    geradoReq.serial_number = nSerie;
-    geradoReq.gender = g;
-    geradoReq.timeReq = t;
-    geradoReq.nRejeitados = 0;
+		geradoReq.serial_number = nSerie;
+		geradoReq.gender = g;
+		geradoReq.timeReq = t;
+		geradoReq.nRejeitados = 0;
 
-    write(fdEnt, &geradoReq, sizeof(geradoReq));
-    //dprintf(STDOUT_FILENO,"inst - %d - %d - %d: %c - %d - SERVIDO\n",getpid(),pthread_self(),geradoReq.serial_number,geradoReq.gender, geradoReq.timeReq);
+		write(fdEnt, &geradoReq, sizeof(geradoReq));
 
-    nSerie++;
-    cont++;
-  }
 
-  close(fdEnt);
+		nSerie++;
+		cont++;
+	}
+
+	close(fdEnt);
 }
 
 //função utilizada para processar os pedidos rejeitados
-void processRejectedRequest(&geradoReq){
+void processRejectedRequest(request *geradoReq){
+	//incrementar antes de verificar
+	geradoReq->nRejeitados++;
+	dprintf(STDOUT_FILENO,"inst - %d - %d - %d: %c - %d - REJEITADO\n",getpid(),pthread_self(),geradoReq->serial_number,geradoReq->gender, geradoReq->timeReq);
+	if(geradoReq->nRejeitados==3){
+		dprintf(STDOUT_FILENO,"inst - %d - %d - %d: %c - %d - DESCARTADO\n",getpid(),pthread_self(),geradoReq->serial_number,geradoReq->gender, geradoReq->timeReq);
+		return -1;
+	}
+	else
+	{
+		int fdEnt;
 
+		fdEnt = open("/tmp/entrada", O_WRONLY);
+
+		write(fdEnt, &geradoReq, sizeof(geradoReq));
+		close(fdEnt);
+		dprintf(STDOUT_FILENO,"inst - %d - %d - %d: %c - %d - PEDIDO\n",getpid(),pthread_self(),geradoReq->serial_number,geradoReq->gender, geradoReq->timeReq);
+	}
 }
 
 void *thr_RejectedRequest(void *arg){
-  int fdRej;
-  //mkfifo("/tmp/rejeitados",0660);
+	int fdRej;
+	//mkfifo("/tmp/rejeitado
+	s",0660);
 
-  fdRej=open("/tmp/rejeitados",O_RDONLY);
+	fdRej=open("/tmp/rejeitados",O_RDONLY);
 
-  if(fdRej == -1){
-    printf("WHY...\n");
-    return 1;
-  }
+	if(fdRej == -1){
+		printf("WHY...\n");
+		return 1;
+	}
 
-  while(true){
-  read(fdRej,&geradoReq,sizeof(geradoReq));
-  processRejectedRequest(&geradoReq);
-  }
+	while(true){
+		read(fdRej,&geradoReq,sizeof(geradoReq));
+		processRejectedRequest(&geradoReq);
+		//condição para parar de ler os rejeitados?
 
-  close(fdRej);
-  unlink("/tmp/rejeitados");
+	}
+
+	close(fdRej);
+	unlink("/tmp/rejeitados");
 }
 
 int main(int argc, char const *argv[]) {
 
-  //tratamento dados
-  if(argc != 4){
-    printf("Usage: geardor <n. Pedidos> <max. utilizacao> <un. tempo>\n");
-    return 1;
-  }
+	//tratamento dados
+	if(argc != 4){
+		printf("Usage: geardor <n. Pedidos> <max. utilizacao> <un. tempo>\n");
+		return 1;
+	}
 
-  int nPedidos;
+	int nPedidos;
 
-  if(atoi(argv[1]) >= 1){
-    nPedidos = atoi(argv[1]);
-  }else{
-    printf("<n. Pedidos> invalid\n");
-    return 1;
-  }
+	if(atoi(argv[1]) >= 1){
+		nPedidos = atoi(argv[1]);
+	}else{
+		printf("<n. Pedidos> invalid\n");
+		return 1;
+	}
 
-  int maxUtilizacao;
+	int maxUtilizacao;
 
-  if(atoi(argv[2]) >= 1){
-    maxUtilizacao = atoi(argv[2]);
-  }else{
-    printf("<max. utilizacao> invalid\n");
-    return 1;
-  }
+	if(atoi(argv[2]) >= 1){
+		maxUtilizacao = atoi(argv[2]);
+	}else{
+		printf("<max. utilizacao> invalid\n");
+		return 1;
+	}
 
-  char unTempo = argv[3][0];
+	char unTempo = argv[3][0];
 
-  //fifo de rejeitados
-  int fdRej;
-  mkfifo("/tmp/rejeitados",0660);
+	//fifo de rejeitados
+	int fdRej;
+	mkfifo("/tmp/rejeitados",0660);
 
-  /*threads*/
-  pthread_t newsRequest;
-  pthread_t rejectedRequest;
+	/*threads*/
+	pthread_t newsRequest;
+	pthread_t rejectedRequest;
 
-  pthread_create(&newsRequest,NULL,thr_NewsRequest,&nPedidos);
-  pthread_create(&rejectedRequest,NULL,thr_RejectedRequest,NULL);
-  /**/
+	pthread_create(&newsRequest,NULL,thr_NewsRequest,&nPedidos);
+	pthread_create(&rejectedRequest,NULL,thr_RejectedRequest,NULL);
+	/**/
 
-  /**/
-  //printf("nPedidos=%d\nmaxUtilizacao=%d\nunTempo=%c\n", nPedidos,maxUtilizacao,unTempo);
-  /**/
+	/**/
+	//printf("nPedidos=%d\nmaxUtilizacao=%d\nunTempo=%c\n", nPedidos,maxUtilizacao,unTempo);
+	/**/
 
-/*
+	/*
   //fifo de entrada
   int fdEnt;
   char str[9999];
-*/
+	 */
 
-  /**/
-  //printf("AQUI 1\n");
-  /**/
+	/**/
+	//printf("AQUI 1\n");
+	/**/
 
-  //fdEnt = open("/tmp/entrada", O_WRONLY);
+	//fdEnt = open("/tmp/entrada", O_WRONLY);
 
-  /**/
-  //printf("AQUI 2 \n");
-  /**/
+	/**/
+	//printf("AQUI 2 \n");
+	/**/
 
-  /**/
-  //printf("AQUI 3\n");
-  /**/
+	/**/
+	//printf("AQUI 3\n");
+	/**/
 
-  /*
+	/*
   fdRej=open("/tmp/rejeitados",O_RDONLY);
 
   if(fdRej == -1){
     printf("WHY...\n");
     return 1;
   }
-  */
+	 */
 
-  /**/
-  //printf("AQUI\n");
-  /**/
+	/**/
+	//printf("AQUI\n");
+	/**/
 
-/*
+	/*
   struct request geradoReq;
 
   //auxiliar de rand
@@ -215,17 +236,17 @@ int main(int argc, char const *argv[]) {
     nSerie++;
     cont++;
   }
-  */
+	 */
 
-  /*
+	/*
   //tratamento de rejeitados//
   read(fdRej,&geradoReq,sizeof(geradoReq));
   //
-  */
+	 */
 
-  //close(fdRej);
-  //close(fdEnt);
-  //unlink("/tmp/rejeitados");
+	//close(fdRej);
+	//close(fdEnt);
+	//unlink("/tmp/rejeitados");
 
-  return 0;
+	return 0;
 }
