6129a50fd64bab97f8619267e6ebec4a7654fa6f
AnabelaSilva <up201506034@fe.up.pt>
2017-05-14 18:35:39 +0100

    Sauna em ingles

--- sauna.c
+++ sauna.c
@@ -4,53 +4,53 @@
 #define NUM_MAX_CLIENTES 10000
 
 
-pthread_cond_t no_vagas = PTHREAD_COND_INITIALIZER;
-int vagas;
-pthread_mutex_t vagas_sem;
-int testar;
-pthread_mutex_t testar_sem;
-int servidos_f=0;
-int servidos_m=0;
+pthread_cond_t no_seats = PTHREAD_COND_INITIALIZER;
+int seats;
+pthread_mutex_t seats_sem;
+int record;
+pthread_mutex_t record_sem;
+int served_f=0;
+int served_m=0;
 struct timespec ts;
-struct timespec tInicial;
+struct timespec tInitial;
 
-void* processamento_de_pedidos(void* pedido){
-    struct request naSauna = *((struct request *) pedido);
-    usleep(naSauna.timeReq*1000);
-    pthread_mutex_lock(&testar_sem);
+void* order_processing(void* pedido){
+    struct request inSauna = *((struct request *) pedido);
+    usleep(inSauna.timeReq*1000);
+    pthread_mutex_lock(&record_sem);
     timespec_get(&ts, TIME_UTC);
-    dprintf(testar,"%8.2f - %d - %ld - %4d: %c - %4d - SERVIDO\n",(ts.tv_sec - tInicial.tv_sec)*1000+(ts.tv_nsec - tInicial.tv_nsec)*0.000001,getpid(),pthread_self(),naSauna.serial_number,naSauna.gender, naSauna.timeReq);
-    if(naSauna.gender=='F'){
-        servidos_f++;
+    dprintf(record,"%8.2f - %d - %ld - %4d: %c - %4d - SERVIDO\n",(ts.tv_sec - tInitial.tv_sec)*1000+(ts.tv_nsec - tInitial.tv_nsec)*0.000001,getpid(),pthread_self(),inSauna.serial_number,inSauna.gender, inSauna.timeReq);
+    if(inSauna.gender=='F'){
+        served_f++;
     }else{
-        servidos_m++;
+        served_m++;
     }
-    pthread_mutex_unlock(&testar_sem);
-    pthread_mutex_lock(&vagas_sem);
-    vagas++;
-    pthread_cond_broadcast(&no_vagas);
-    pthread_mutex_unlock(&vagas_sem);
+    pthread_mutex_unlock(&record_sem);
+    pthread_mutex_lock(&seats_sem);
+    seats++;
+    pthread_cond_broadcast(&no_seats);
+    pthread_mutex_unlock(&seats_sem);
     return NULL;
 }
 
 int main(int argc, char** argv){
-    timespec_get(&tInicial, TIME_UTC);
-    //Validação de variaveis
+    timespec_get(&tInitial, TIME_UTC);
+    //Variable validation
     if(argc != 2){
         printf("Usage: sauna <n. lugares>\n");
         return 1;
     }
-    //Inicialização de mutexes e variaveis de condição
-    pthread_mutex_init(&vagas_sem,NULL);
-    pthread_mutex_init(&testar_sem,NULL);
-    pthread_cond_init(&no_vagas,NULL);
-    //Inicialização do numero de lugares
-    int nLugares = atoi(argv[1]);
-    pthread_mutex_lock(&vagas_sem);
-    vagas=nLugares;
-    pthread_mutex_unlock(&vagas_sem);
+    //Initialization of mutexes and condition variables
+    pthread_mutex_init(&seats_sem,NULL);
+    pthread_mutex_init(&record_sem,NULL);
+    pthread_cond_init(&no_seats,NULL);
+    //Initialization of the number of seats
+    int nSeats = atoi(argv[1]);
+    pthread_mutex_lock(&seats_sem);
+    seats=nSeats;
+    pthread_mutex_unlock(&seats_sem);
 
-    //Criação e abretura de fifos
+    //Creation and opening of fifos
     if(mkfifo("/tmp/entrada",0660)){
         perror("sauna");
         printf("Erro ao criar FIFO - /tmp/entrada\n");
@@ -67,106 +67,104 @@ int main(int argc, char** argv){
         fifo_rejeitados= open("/tmp/rejeitados",O_WRONLY);
     }while(fifo_rejeitados==-1);
 
-    //Criação e abretura de ficheiro de registo
-    char nomeFile[NUM_CHARS_FILE_NAME];
-    sprintf(nomeFile, "%s%d","/tmp/bal.", getpid());
-    pthread_mutex_lock(&testar_sem);
-    if((testar = open(nomeFile,O_WRONLY | O_CREAT | O_EXCL,0666)) == -1){
+    //Creation and opening of log file
+    char nameOfFile[NUM_CHARS_FILE_NAME];
+    sprintf(nameOfFile, "%s%d","/tmp/bal.", getpid());
+    pthread_mutex_lock(&record_sem);
+    if((record = open(nameOfFile,O_WRONLY | O_CREAT | O_EXCL,0666)) == -1){
         perror("sauna");
-        pthread_mutex_unlock(&testar_sem);
-        printf("Erro ao abrir FILE - %s\n",nomeFile);
+        pthread_mutex_unlock(&record_sem);
+        printf("Erro ao abrir FILE - %s\n",nameOfFile);
         return 1;
     }
-    pthread_mutex_unlock(&testar_sem);
+    pthread_mutex_unlock(&record_sem);
 
-    //TODO PROCESSAMENTO
-
-    //Processamento dos Pedidos
-    char genero='G';
-    struct request recebido[NUM_MAX_CLIENTES];
+    //Order Processing
+    char gender='G';
+    struct request received[NUM_MAX_CLIENTES];
     pthread_t tid[NUM_MAX_CLIENTES];
     int i=0;
-    //Variaveis para as estatisticas
-    int recebidos_f=0;
-    int recebidos_m=0;
-    int rejeitados_f=0;
-    int rejeitados_m=0;
+    //Variables for statistics
+    int received_f=0;
+    int received_m=0;
+    int rejected_f=0;
+    int rejected_m=0;
 
     while(i<NUM_MAX_CLIENTES){
-        //Recebe pedido
-        if(read(fifo_entrada,&(recebido[i]),sizeof(recebido[0]))==0)break;
-        pthread_mutex_lock(&testar_sem);
+        //Receive request
+        if(read(fifo_entrada,&(received[i]),sizeof(received[0]))==0)break;
+        pthread_mutex_lock(&record_sem);
         timespec_get(&ts, TIME_UTC);
-        dprintf(testar,"%8.2f - %d - %ld - %4d: %c - %4d - RECEBIDO\n",(ts.tv_sec - tInicial.tv_sec)*1000+(ts.tv_nsec - tInicial.tv_nsec)*0.000001,getpid(),pthread_self(),recebido[i].serial_number,recebido[i].gender, recebido[i].timeReq);
-        if(recebido[i].gender=='F'){
-            recebidos_f++;
+        dprintf(record,"%8.2f - %d - %ld - %4d: %c - %4d - RECEBIDO\n",(ts.tv_sec - tInitial.tv_sec)*1000+(ts.tv_nsec - tInitial.tv_nsec)*0.000001,getpid(),pthread_self(),received[i].serial_number,received[i].gender, received[i].timeReq);
+        if(received[i].gender=='F'){
+            received_f++;
         }else{
-            recebidos_m++;
+            received_m++;
         }
-        pthread_mutex_unlock(&testar_sem);
+        pthread_mutex_unlock(&record_sem);
 
-        //Se ninguem estiver na Sauna, atualiza genero
-        pthread_mutex_lock(&vagas_sem);
-        if(vagas==nLugares){
-            pthread_mutex_unlock(&vagas_sem);
-            genero=recebido[i].gender;
+        //If nobody is in the Sauna, update gender
+        pthread_mutex_lock(&seats_sem);
+        if(seats==nSeats){
+            pthread_mutex_unlock(&seats_sem);
+            gender=received[i].gender;
         }else{
-            pthread_mutex_unlock(&vagas_sem);
+            pthread_mutex_unlock(&seats_sem);
         }
 
-        //Se na sauna estiverem pessoas de outro genero, rejeita
-        if(genero!=recebido[i].gender){
-             pthread_mutex_lock(&testar_sem);
+        //If in the sauna are people of another gender, reject
+        if(gender!=received[i].gender){
+             pthread_mutex_lock(&record_sem);
              timespec_get(&ts, TIME_UTC);
-             dprintf(testar,"%8.2f - %d - %ld - %4d: %c - %4d - REJEITADO\n",(ts.tv_sec - tInicial.tv_sec)*1000+(ts.tv_nsec - tInicial.tv_nsec)*0.000001,getpid(),pthread_self(),recebido[i].serial_number,recebido[i].gender, recebido[i].timeReq);
-             if(recebido[i].gender=='F'){
-                rejeitados_f++;
+             dprintf(record,"%8.2f - %d - %ld - %4d: %c - %4d - REJEITADO\n",(ts.tv_sec - tInitial.tv_sec)*1000+(ts.tv_nsec - tInitial.tv_nsec)*0.000001,getpid(),pthread_self(),received[i].serial_number,received[i].gender, received[i].timeReq);
+             if(received[i].gender=='F'){
+                rejected_f++;
              }else{
-                rejeitados_m++;
+                rejected_m++;
              }
-             pthread_mutex_unlock(&testar_sem);
-             write(fifo_rejeitados,&(recebido[i]),sizeof(recebido[0]));
+             pthread_mutex_unlock(&record_sem);
+             write(fifo_rejeitados,&(received[i]),sizeof(received[0]));
         }else
         {
-            //Se existem vagas, entra na sauna
+            //If there are seats, enter the sauna
             while(1){
-                pthread_mutex_lock(&vagas_sem);
-                if(vagas>0){
-                    vagas--;
-                    pthread_mutex_unlock(&vagas_sem);
-                    pthread_create(&(tid[i]), NULL, &processamento_de_pedidos, &(recebido[i]));
+                pthread_mutex_lock(&seats_sem);
+                if(seats>0){
+                    seats--;
+                    pthread_mutex_unlock(&seats_sem);
+                    pthread_create(&(tid[i]), NULL, &order_processing, &(received[i]));
                     break;
                 }else{
-                    //Espera que existam vagas
-                    pthread_cond_wait(&no_vagas,&vagas_sem);
-                    pthread_mutex_unlock(&vagas_sem);
+                    //Waits for seats
+                    pthread_cond_wait(&no_seats,&seats_sem);
+                    pthread_mutex_unlock(&seats_sem);
                 }
             }
             i++;
         }
     }
-    //Espera pela conclusão das threads
+    //Wait for the threads to complete
     int num_tids=i;
     for(i=0;i<num_tids;i++){
         pthread_join(tid[i],NULL);
     }
 
-    //Imprime estatisticas
-    pthread_mutex_lock(&testar_sem);
+    //Print statistics
+    pthread_mutex_lock(&record_sem);
     dprintf(STDOUT_FILENO,"N. recebidos:\n%d, %d, %d\nN. rejeitados:\n%d, %d, %d\nN. servidos:\n%d, %d, %d\n"
-            ,(recebidos_f+recebidos_m),recebidos_f,recebidos_m
-            ,(rejeitados_f+rejeitados_m),rejeitados_f,rejeitados_m
-            ,(servidos_f+servidos_m),servidos_f,servidos_m);
-    pthread_mutex_unlock(&testar_sem);
+            ,(received_f+received_m),received_f,received_m
+            ,(rejected_f+rejected_m),rejected_f,rejected_m
+            ,(served_f+served_m),served_f,served_m);
+    pthread_mutex_unlock(&record_sem);
 
-    //Fechar as variaveis
+    //Close the variables
     close(fifo_rejeitados);
-    pthread_mutex_lock(&testar_sem);
-    close(testar);
-    pthread_mutex_unlock(&testar_sem);
-    pthread_mutex_destroy(&testar_sem);
-    pthread_mutex_destroy(&vagas_sem);
-    pthread_cond_destroy(&no_vagas);
+    pthread_mutex_lock(&record_sem);
+    close(record);
+    pthread_mutex_unlock(&record_sem);
+    pthread_mutex_destroy(&record_sem);
+    pthread_mutex_destroy(&seats_sem);
+    pthread_cond_destroy(&no_seats);
     close(fifo_entrada);
     unlink("/tmp/entrada");
     return 0;
